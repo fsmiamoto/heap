9caa297206d39c757015e854cfec65a653d8e3ce
Francisco Miamoto <fsmiamoto@gmail.com>
2020-04-14 22:23:58 -0300

    implements insertion on the heap
    Also extracts the heapify logic from the New function

--- heap.go
+++ heap.go
@@ -1,71 +1,90 @@
 package heap
 
+import "errors"
+
 type Heap struct {
 	size     int
 	capacity int
+	compare  CompareFunc
 	elements []interface{}
 }
 
 type CompareFunc func(node, child interface{}) bool
 
-func New(elements []interface{}, capacity int, c CompareFunc) *Heap {
-	h := &Heap{
-		size:     len(elements),
-		capacity: capacity,
-	}
-
+func New(elements []interface{}, capacity int, cf CompareFunc) *Heap {
+	// Make a copy of the original elements for no bad surprises
 	elems := make([]interface{}, capacity)
 	copy(elems, elements)
-
-	i := len(elems)/2 - 1
-	for i >= 0 {
-		left := 2*i + 1
-		right := 2*i + 2
-
-		switch {
-		case h.validIndex(left) && !h.validIndex(right):
-			shouldSwap := c(elems[i], elems[left])
-			if shouldSwap {
-				elems[i], elems[left] = elems[left], elems[i]
-				if left < len(elems)/2 {
-					i = left + 1
-				}
-			}
-		case h.validIndex(left) && h.validIndex(right):
-			rightIsLarger := c(elems[left], elems[right])
-			var compareIndex int
-
-			if rightIsLarger {
-				compareIndex = right
-			} else {
-				compareIndex = left
-			}
-
-			shouldSwap := c(elems[i], elems[compareIndex])
-			if shouldSwap {
-				elems[i], elems[compareIndex] = elems[compareIndex], elems[i]
-				if compareIndex < len(elems)/2 {
-					i = compareIndex + 1
-				}
-			}
-		}
-		i--
+	heapify(elems, cf)
+	return &Heap{
+		size:     len(elems),
+		capacity: capacity,
+		compare:  cf,
+		elements: elems,
 	}
-
-	h.elements = elems
-	return h
 }
 
-func (h *Heap) Insert(x interface{}) {
-	return
+func (h *Heap) Insert(x interface{}) error {
+	if h.size == h.capacity {
+		return errors.New("heap has no capacity")
+	}
+
+	h.size++
+	h.elements[h.size-1] = x
+
+	// Fix the heap
+	i := h.size - 1
+	for i >= 0 && h.compare(h.elements[parent(i)], h.elements[i]) {
+		h.elements[parent(i)], h.elements[i] = h.elements[i], h.elements[parent(i)]
+		i = parent(i)
+	}
+
+	return nil
 }
 
 func (h *Heap) DeleteRoot() interface{} {
 	return nil
 }
 
-func (h *Heap) validIndex(i int) bool {
-	return i < h.size
+// heapify makes a heap of the slice in-place
+// TODO: review this logic
+func heapify(elems []interface{}, compare CompareFunc) {
+	i := len(elems)/2 - 1
+	for i >= 0 {
+		left := 2*i + 1
+		right := 2*i + 2
+
+		if right > len(elems)-1 {
+			// Look at only the left child
+			shouldSwap := compare(elems[i], elems[left])
+			if shouldSwap {
+				elems[i], elems[left] = elems[left], elems[i]
+				if left < len(elems)/2 {
+					i = left + 1
+				}
+			}
+			continue
+		}
+
+		rightIsLarger := compare(elems[left], elems[right])
+		var compareIndex int
+
+		if rightIsLarger {
+			compareIndex = right
+		} else {
+			compareIndex = left
+		}
+
+		shouldSwap := compare(elems[i], elems[compareIndex])
+		if shouldSwap {
+			elems[i], elems[compareIndex] = elems[compareIndex], elems[i]
+			if compareIndex < len(elems)/2 {
+				i = compareIndex + 1
+			}
+			continue
+		}
+		i--
+	}
 }
 
 func parent(i int) int {
--- heap_test.go
+++ heap_test.go
@@ -37,4 +37,75 @@ func TestHeap(t *testing.T) {
 			t.Errorf("Expected %v but got %v", tt.expect, heap.elements)
 		}
 	}
+
+	t.Run("Test Insert", func(t *testing.T) {
+		tests := []struct {
+			name       string
+			element    interface{}
+			cf         CompareFunc
+			shouldFail bool
+			heap       *Heap
+			expect     *Heap
+		}{
+			{"Insert on MaxHeap", 60, max, false,
+				&Heap{
+					size:     7,
+					capacity: 9,
+					compare:  max,
+					elements: []interface{}{50, 30, 20, 15, 10, 8, 16, 0, 0},
+				},
+				&Heap{
+					size:     8,
+					capacity: 9,
+					compare:  max,
+					elements: []interface{}{60, 50, 20, 30, 10, 8, 16, 15, 0},
+				},
+			},
+			{"Heap already full", 60, max, true,
+				&Heap{
+					size:     9,
+					capacity: 9,
+					compare:  max,
+					elements: []interface{}{50, 30, 20, 15, 10, 8, 16, 9, 8},
+				},
+				&Heap{
+					size:     9,
+					capacity: 9,
+					compare:  max,
+					elements: []interface{}{50, 30, 20, 15, 10, 8, 16, 9, 8},
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			err := tt.heap.Insert(tt.element)
+			assertError(t, tt.shouldFail, err)
+			assertEqualHeap(t, tt.heap, tt.expect)
+
+		}
+	})
+}
+
+func assertError(t *testing.T, shouldFail bool, err error) {
+	t.Helper()
+	if shouldFail && err == nil {
+		t.Errorf("a error was expected but got none")
+	}
+
+	if !shouldFail && err != nil {
+		t.Errorf("no error was expected but got %v", err)
+	}
+}
+
+func assertEqualHeap(t *testing.T, got, want *Heap) {
+	t.Helper()
+	if got.size != want.size {
+		t.Errorf("heaps have different sizes, got %d want %d", got.size, want.size)
+	}
+	if got.capacity != want.capacity {
+		t.Errorf("heaps have different capacities, got %d want %d", got.capacity, want.capacity)
+	}
+	if !reflect.DeepEqual(got.elements, want.elements) {
+		t.Errorf("heaps have different elements, got %v want %v", got.elements, want.elements)
+	}
 }
